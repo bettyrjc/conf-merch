263e6febca44187f0b063d1dbc1eaf0f4d94eddc
Betty Jimenez <bettyjimenez3010@gmail.com>
2022-01-11 21:03:12 -0400

    :art: add meta tag

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -6,6 +6,26 @@ import Products from '../components/Products';
 const Home = () => {
   <Helmet>
     <title>Esto es platzi conf merch - productos</title>
+    <meta name="twitter:card" content="summary_large_image" />
+    <meta name="twitter:site" content="@bettyrjce" />
+    <meta name="twitter:creator" content="@bettyrjce" />
+    <meta name="twitter:title" content="Platzi Conf Store" />
+    <meta name="twitter:description" content="Platzi Conf Store" />
+    {/* <meta
+      name="twitter:image"
+      content="https://s3.amazonaws.com/gndx.dev/gndxdev.png"
+    /> */}
+    <meta property="og:title" content="Platzi Conf Store" />
+    <meta property="og:description" content="Platzi Conf Store" />
+    {/* <meta
+      property="og:image"
+      content="https://s3.amazonaws.com/gndx.dev/gndxdev.png"
+    /> */}
+    {/* <meta property="og:url" content="platzistore.xyz" /> */}
+    <meta property="og:site_name" content="Platzi Conf Store" />
+    <meta property="og:locale" content="es_ES" />
+    <meta property="og:type" content="article" />
+    {/* <meta property="fb:app_id" content="ID_APP_FACEBOOK" /> */}
   </Helmet>;
   return <Products />;
 };
