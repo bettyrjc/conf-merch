cdd90e9e7066751b8813131c8da9f20e326f591b
Betty Jimenez <bettyjimenez3010@gmail.com>
2022-01-11 20:58:03 -0400

    :sparkles: add helmet

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
 import Products from '../components/Products';
 // import initialState from '../initialState';
 
 const Home = () => {
-  return (
-    <Products  />
-  );
-}
+  <Helmet>
+    <title>Esto es platzi conf merch - productos</title>
+  </Helmet>;
+  return <Products />;
+};
 
 export default Home;
